f6a86381fa0796b610a62948496ab1b5b5b649bd
Chayapol Moemeng <mchayapol@gmail.com>
2015-09-09 11:20:51 +0700

    Add selectionSort() Add insertionSort() skeleton

--- week05_sorting/src/edu/au/scitech/sc2101/SortUtility.java
+++ week05_sorting/src/edu/au/scitech/sc2101/SortUtility.java
@@ -3,6 +3,35 @@ package edu.au.scitech.sc2101;
 import java.util.Arrays;
 
 public class SortUtility {
+	public static void insertionSort(int[] arr) {
+		
+	}
+	
+	public static void selectionSort(int[] arr){
+		System.out.println(Arrays.toString(arr));
+		int counter = 0;
+		boolean swapRequired = false;
+		for(int i = 0 ; i < arr.length-1; i++){
+			int minArrIndex = i;
+			swapRequired = false;
+			for(int j = i+1; j < arr.length; j++){
+				if(arr[j] < arr[minArrIndex]){
+					minArrIndex = j;
+					swapRequired = true;
+				}	
+				counter++;
+			}
+			if (swapRequired) {
+				int temp = arr[i];
+				arr[i] = arr[minArrIndex];
+				arr[minArrIndex]= temp;
+				System.out.println(Arrays.toString(arr));
+			} 
+		}	
+		System.out.println(Arrays.toString(arr));		
+		System.out.println("#"+counter);
+	}
+	
 	/* Make the method static, because it doesn't
 	 * require state of data. 
 	 * It is called once and done. 
--- week05_sorting/src/edu/au/scitech/sc2101/SortUtilityTest.java
+++ week05_sorting/src/edu/au/scitech/sc2101/SortUtilityTest.java
@@ -9,6 +9,48 @@ import org.junit.Test;
 public class SortUtilityTest {
 
 	@Test
+	public void testInsertSort() {
+		int[] worstData = {7, 6, 5, 4, 3, 2, 1};
+		int[] bestData = {1, 2, 3, 4, 5, 6, 7};
+		int[] randomData1 = {2, 1, 3, 6, 7, 5, 4};
+		int[] randomData2 = {3, 1, 4, 2, 7, 5, 6};
+		int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+		SortUtility.insertionSort(worstData);		
+		assertArrayEquals(expected, worstData);
+
+		SortUtility.insertionSort(bestData);		
+		assertArrayEquals(expected, bestData);
+
+		SortUtility.insertionSort(randomData1);		
+		assertArrayEquals(expected, randomData1);
+
+		SortUtility.insertionSort(randomData2);		
+		assertArrayEquals(expected, randomData2);
+	}
+	
+	//@Test
+	public void testSelectionSort() {
+		int[] worstData = {7, 6, 5, 4, 3, 2, 1};
+		int[] bestData = {1, 2, 3, 4, 5, 6, 7};
+		int[] randomData1 = {2, 1, 3, 6, 7, 5, 4};
+		int[] randomData2 = {3, 1, 4, 2, 7, 5, 6};
+		int[] expected = {1, 2, 3, 4, 5, 6, 7};
+
+		SortUtility.selectionSort(worstData);		
+		assertArrayEquals(expected, worstData);
+
+		SortUtility.selectionSort(bestData);		
+		assertArrayEquals(expected, bestData);
+
+		SortUtility.selectionSort(randomData1);		
+		assertArrayEquals(expected, randomData1);
+
+		SortUtility.selectionSort(randomData2);		
+		assertArrayEquals(expected, randomData2);
+	}
+	
+	//@Test
 	public void testBubbleSort() {
 		int[] worstData = {7, 6, 5, 4, 3, 2, 1};
 		int[] bestData = {1, 2, 3, 4, 5, 6, 7};
